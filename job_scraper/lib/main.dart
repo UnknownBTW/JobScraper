705b5609120546fbc6c6b6c38aebefd6b58bb88b
UnknownBTW <64477446+UnknownBTW@users.noreply.github.com>
2024-08-08 20:33:14 +0100

    Progress
    Update app title to search value. Added JobScraper logo in the top left

--- job_scraper/lib/main.dart
+++ job_scraper/lib/main.dart
@@ -27,62 +27,80 @@ class _JobScraperState extends State<JobScraper> {
 
   @override
   Widget build(BuildContext context){
-    const appTitle = 'JobScraper';
+    String appTitle = 'JobScraper';
     return MaterialApp(
       title: appTitle,
       home: Builder(
         builder: (context) => Scaffold(
           backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        body: Stack(
-          children: [
-            Center(
-              child: SizedBox(
-                height: 300,
-                child: Text(
-                  'JobScraper',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
+          body: Stack(
+            children: [
+              Center(
+                child: SizedBox(
+                  height: 300,
+                  child: Text(
+                    'JobScraper',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Center(
-              child: SizedBox(
-                width: 700,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextField(
-                    controller: myController,
-                    onSubmitted: (String value) {
-                      print(value);
-                      _navigateToNextScreen(context);
-                    },
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Enter a search term',
-                    ),
+              Center(
+                child: SizedBox(
+                  width: 700,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextField(
+                      controller: myController,
+                      onSubmitted: (String value) {
+                        print(value);
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => SearchResult(value)));
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter a search term',
+                      ),
+                    )
                   )
-                )
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
         ),
       ),
     );
   }
-  void _navigateToNextScreen(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => SearchResult()));
-  }
 }
 
 class SearchResult extends StatelessWidget{
+  final String value;
+  SearchResult(this.value);
   @override
   Widget build( BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('New Screen'),),
+    return MaterialApp(
+      title: value,
+      home: Builder(
+        builder: (context) => Scaffold(
+          backgroundColor: Colors.white,
+          body: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                child: Text(
+                  'JobScraper',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
